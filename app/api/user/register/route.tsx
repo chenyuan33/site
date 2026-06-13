4a0610281e9db56b1eb9821da8860401b44e56aa
chenyuan33 <115919187+chenyuan33@users.noreply.github.com>
2026-06-13 11:23:04 +0800

    test

---
+++ app/api/user/register/route.tsx
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+export const runtime = 'edge';
+export async function GET(): Promise<NextResponse> {
+	return NextResponse.json({
+		msg: 'this is only testing GET. your request have been ignored.'
+	});
+}
+export async function POST(request: NextRequest): Promise<NextResponse> {
+	return NextResponse.json({
+		msg: 'this is only testing POST. your request have been ignored.',
+		sth: await request.json()
+	});
+}
